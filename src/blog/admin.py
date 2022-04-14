3009f963705d180c66c4f2c389e705f8e6eb0a8b
Shrikant Dhayje <35664193+shriekdj@users.noreply.github.com>
2022-04-14 13:55:15 +0000

    add series to admin panel

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tag, Post
+from .models import Tag, Series, Post
 
 admin.site.register(Tag)
+admin.site.register(Series)
 admin.site.register(Post)
